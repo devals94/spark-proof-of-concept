d12950873e698c14e2441f8a4876cf16e2d5daed
Deval Shah <devals94@gmail.com>
2017-09-01 10:56:34 +0530

    Update ScalaCode7.scala

--- Spark-Core/Scala/Codes/scala/ScalaCode7.scala
+++ Spark-Core/Scala/Codes/scala/ScalaCode7.scala
@@ -1,6 +1,5 @@
 val SampleData = SplitData.map( x => (x(5),(x(11))) )
-val FilteredByRuns = SampleData.filter( x => (x._2 != "by wickets" && x._2 != "Tie" && x._2 !=
-"No Result"))
+val FilteredByRuns = SampleData.filter( x => (x._2 != "by wickets" && x._2 != "Tie" && x._2 !="No Result"))
 val ReduceByRuns = FilteredByRuns.map( x => (x._1,1)).reduceByKey(_+_)
 val SwapByRuns = ReduceByRuns.map( item => item.swap)
 val SortByRuns = SwapByRuns.sortByKey( ascending = false)
