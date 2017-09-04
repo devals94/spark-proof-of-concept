b69e029482c03c4ada3911dd62449aee516fb3f4
Deval Shah <devals94@gmail.com>
2017-09-04 11:29:27 +0530

    Add files via upload

---
+++ Spark-Core/Python/Codes/python/PythonCode4.py
@@ -0,0 +1,4 @@
+UP2017data = sc.textFile("/project/UpAssembly").map(lambda line : line.split(","))
+candidatesCount = UP2017data.filter(lambda line: line[6] == 'INC').map(lambda line: ((line[4]),1))
+districtCountofINC = candidatesCount.reduceByKey(lambda a,b: a+b)
+districtCountofINC.sortByKey(False).collect()
